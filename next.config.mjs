d1689aa8f16c9b2ccf1e4e707648710e46a2066d
pankjava <155135907+pankjava@users.noreply.github.com>
2026-03-19 13:49:10 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Ye line Vercel ko bolegi ki ESLint errors hone par bhi build fail na kare
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Ye line Vercel ko bolegi ki TypeScript errors hone par bhi build fail na kare
     ignoreBuildErrors: true,
   },
 };
 
-module.exports = nextConfig; 
-// Note: Agar file .mjs hai toh 'export default nextConfig;' use karein
+module.exports = nextConfig;
